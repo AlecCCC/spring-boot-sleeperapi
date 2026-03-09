e656b4ecbd959d4d55cfc648dc12192249a628d3
Alec <alecjames1996@aol.com>
2026-03-09 14:34:19 -0400

    Read nflplayer data into file

--- src/main/java/com/sleeperapi/sleeperapi/service/NFLPlayerData.java
+++ src/main/java/com/sleeperapi/sleeperapi/service/NFLPlayerData.java
@@ -1,17 +1,50 @@
 package com.sleeperapi.sleeperapi.service;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
-@EnableScheduling
 public class NFLPlayerData {
 
-    RestClient restClient;
+   private final RestClient restClient;
+   private final ObjectMapper objectMapper;
 
-    public NFLPlayerData(RestClient restClient) {
+    private Map<String, Object> players = new HashMap<>();
+
+
+    public NFLPlayerData(RestClient restClient, ObjectMapper objectMapper) {
         this.restClient = restClient;
+        this.objectMapper = objectMapper;
     }
+
+    @PostConstruct
+    public void loadPlayers() throws Exception {
+        File file = new File("players.json");
+        if (file.exists()) {
+            players = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() {});
+        } else {
+            fetchAndSavePlayers();
+        }
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void fetchAndSavePlayers() throws Exception {
+        players = restClient.get()
+                .uri("/players/nfl")
+                .retrieve()
+                .body(new ParameterizedTypeReference<Map<String, Object>>() {});
+
+        objectMapper.writeValue(new File("players.json"), players);
+    }
+
 
 }
