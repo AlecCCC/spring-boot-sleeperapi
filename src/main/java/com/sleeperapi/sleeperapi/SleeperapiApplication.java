24551e8b62c0facefaa6a149f6dae181c3bff871
Alec <alecjames1996@aol.com>
2026-03-10 17:00:57 -0400

    Scheduling

--- src/main/java/com/sleeperapi/sleeperapi/SleeperapiApplication.java
+++ src/main/java/com/sleeperapi/sleeperapi/SleeperapiApplication.java
@@ -2,8 +2,10 @@ package com.sleeperapi.sleeperapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SleeperapiApplication {
 
 	public static void main(String[] args) {
